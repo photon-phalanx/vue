05b5cbe83944b63a2af53c203bdde812d99ea275
photon-phalanx <photonphalanx@gmail.com>
2018-08-26 20:05:44 +0800

    commit

--- src/platforms/web/runtime/patch.js
+++ src/platforms/web/runtime/patch.js
@@ -9,4 +9,6 @@ import platformModules from 'web/runtime/modules/index'
 // built-in modules have been applied.
 const modules = platformModules.concat(baseModules)
 
+// 这里的modules是 src/platforms/web/runtime/modules,是一些和web有关的东西
+// 而nodeOps是dom操作的一些方法，这里这样做就保证了很好的跨平台解耦。
 export const patch: Function = createPatchFunction({ nodeOps, modules })
